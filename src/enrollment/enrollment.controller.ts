af3a32a1daee35018158cc9e89c874d2e3f9f82e
Kevin Kim <kimanikevin254@gmail.com>
2024-07-02 12:46:30 +0300

    enrollments CRUD

--- src/enrollment/enrollment.controller.ts
+++ src/enrollment/enrollment.controller.ts
@@ -3,6 +3,8 @@ import { AuthGuard } from 'src/auth/auth.guard';
 import { EnrollmentDto } from './dto/enrollment.dto';
 import { EnrollmentInterface } from './interface/enrollment.interface';
 import { EnrollmentService } from './enrollment.service';
+import { Enrollment } from '@prisma/client';
+import { EnrollmentListInterface } from './interface/enrollment-list.interface';
 
 @Controller('enrollment')
 @UseGuards(AuthGuard)
@@ -13,19 +15,19 @@ export class EnrollmentController {
 
     @Post()
     @HttpCode(201)
-    async create(@Body() enrollmentDto: EnrollmentDto, @Req() req: any): Promise<any>{
+    async create(@Body() enrollmentDto: EnrollmentDto, @Req() req: any): Promise<Enrollment>{
         return await this.enrollmentService.create(enrollmentDto, req.user.sub)
     }
 
     @Get()
     @HttpCode(200)
-    async findAll( @Req() req: any){
+    async findAll( @Req() req: any): Promise<EnrollmentListInterface[]>{
         return await this.enrollmentService.findAll(req.user.sub)
     }
 
     @Get(':enrollmentId')
     @HttpCode(200)
-    async findOne(@Param('enrollmentId') enrollmentId: string, @Req() req: any){
+    async findOne(@Param('enrollmentId') enrollmentId: string, @Req() req: any): Promise<EnrollmentInterface>{
         return await this.enrollmentService.findOne(enrollmentId, req.user.sub)
     }
 }
